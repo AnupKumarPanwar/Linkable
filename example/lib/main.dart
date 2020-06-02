a5cfa74cd85ca94767e19e1e7eba1c8a170a8c67
Anup Kumar Panwar <1anuppanwar@gmail.com>
2020-06-02 19:06:01 +0530

    update README

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,20 +10,13 @@ class HypertextExample extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Hypertext example',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hypertext example'),
-        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Center(
               child: Hypertext(
-                textAlign: TextAlign.center,
                 text:
                     "Hi!\nI'm Anup.\n\nYou can email me at 1anuppanwar@gmail.com.\nOr just whatsapp me @ +91-8968894728.\n\nFor more info visit: \ngithub.com/anupkumarpanwar \nor\nhttps://www.linkedin.com/in/anupkumarpanwar/",
-                style: TextStyle(fontSize: 20),
-                textColor: Colors.black,
-                linkColor: Colors.red,
               ),
             ),
           ],
